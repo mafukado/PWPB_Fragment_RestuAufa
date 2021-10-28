f9abccede262929536807cdde3f4ca7546aca6e9
restu <restuaufacahyadin02@gmail.com>
2021-10-28 18:03:10 +0700

    tugas latihan fragment selesai

---
+++ Restu_Fragment/app/src/main/java/com/example/restu_fragment/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,24 @@
+package com.example.restu_fragment.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.restu_fragment.R;
+import com.example.restu_fragment.databinding.FragmentDashboardBinding;
+
+public class DashboardFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_dashboard, null);
+    }
+}
---
+++ Restu_Fragment/app/src/main/java/com/example/restu_fragment/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,25 @@
+package com.example.restu_fragment.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.restu_fragment.R;
+import com.example.restu_fragment.databinding.FragmentNotificationsBinding;
+
+public class NotificationsFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup
+
+            container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notifications, null);
+    }
+}
